cae658d0291869a1bbbd894052256d68df3576ac
Tikondra <tikondra47@gmail.com>
2020-03-31 18:17:44 +0400

    Исправила перечисления

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ const TASK_COUNT = 3;
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 const Place = {
-  afterbegin: `afterbegin`,
-  beforeend: `beforeend`
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
 };
 
 const createSiteMenu = () => (
@@ -358,21 +358,21 @@ const createButtonMore = () => (
 );
 const renderCard = (container, count) => {
   for (let i = 0; i < count; i++) {
-    render(container, createTask(), Place.beforeend);
+    render(container, createTask(), Place.BEFOREEND);
   }
 };
 const render = (container, template, place) => container.insertAdjacentHTML(place, template);
 const init = () => {
-  render(siteHeaderElement, createSiteMenu(), Place.beforeend);
-  render(siteMainElement, createFilter(), Place.beforeend);
-  render(siteMainElement, createBoard(), Place.beforeend);
+  render(siteHeaderElement, createSiteMenu(), Place.BEFOREEND);
+  render(siteMainElement, createFilter(), Place.BEFOREEND);
+  render(siteMainElement, createBoard(), Place.BEFOREEND);
 
   const taskListElement = siteMainElement.querySelector(`.board__tasks`);
   const boardElement = siteMainElement.querySelector(`.board`);
 
-  render(taskListElement, createTaskEdit(), Place.beforeend);
+  render(taskListElement, createTaskEdit(), Place.BEFOREEND);
   renderCard(taskListElement, TASK_COUNT);
-  render(boardElement, createButtonMore(), Place.beforeend);
+  render(boardElement, createButtonMore(), Place.BEFOREEND);
 };
 
 init();
