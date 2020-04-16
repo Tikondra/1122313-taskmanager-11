f9fce560525a01125db9731944aae26554f53b70
Tikondra <tikondra47@gmail.com>
2020-04-16 23:10:12 +0400

    Добавит функцию для создания DOM-элемента

--- src/components/utils.js
+++ src/components/utils.js
@@ -51,3 +51,10 @@ export const getDataTask = (dueDate, repeatingDays) => {
     isRepeatingTask,
   };
 };
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
