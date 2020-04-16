13da5ee58d258218bc832d63ada14d37130bf672
Tikondra <tikondra47@gmail.com>
2020-04-16 23:41:12 +0400

    создает компоненты menu, buttonMore

--- src/components/button-more.js
+++ src/components/button-more.js
@@ -1,1 +1,25 @@
-export const createButtonMore = () => (`<button class="load-more" type="button">load more</button>`);
+import {createElement} from "./utils";
+
+const createButtonMore = () => (`<button class="load-more" type="button">load more</button>`);
+
+export default class ButtonMore {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createButtonMore();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
