64ab5560fa8d4e90b6587cb3a6d50b3a79fae689
Tikondra <tikondra47@gmail.com>
2020-05-10 18:20:21 +0400

    подключает статистику

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import BoardComponent from "./components/board.js";
 import FilterController from "./controllers/filter";
 import SiteMenuComponent from "./components/menu.js";
+import StatisticsComponent from "./components/statistics";
 import BoardController from "./controllers/board";
 import TasksModel from "./models/tasks.js";
 
@@ -33,11 +34,31 @@ const boardController = new BoardController(boardComponent, tasksModel);
 
 boardController.render();
 
+const dateTo = new Date();
+const dateFrom = (() => {
+  const d = new Date(dateTo);
+  d.setDate(d.getDate() - 7);
+  return d;
+})();
+const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
+render(siteMainElement, statisticsComponent, Place.BEFOREEND);
+statisticsComponent.hide();
+
 siteMenuComponent.setOnChange((menuItem) => {
   switch (menuItem) {
     case MenuItem.NEW_TASK:
       siteMenuComponent.setActiveItem(MenuItem.TASKS);
+      statisticsComponent.hide();
+      boardController.show();
       boardController.createTask();
+      break;
+    case MenuItem.STATISTICS:
+      boardController.hide();
+      statisticsComponent.show();
+      break;
+    case MenuItem.TASKS:
+      statisticsComponent.hide();
+      boardController.show();
       break;
   }
 });
