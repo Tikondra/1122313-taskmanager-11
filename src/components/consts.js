423ce86b4445ca08990ad57dc87b2a3a43cc22a3
Tikondra <tikondra47@gmail.com>
2020-05-20 21:46:14 +0400

    html

--- src/components/consts.js
+++ src/components/consts.js
@@ -43,7 +43,7 @@ export const OptionTasks = {
 export const ApiOption = {
   TASKS: `tasks`,
   END_POINT: `https://11.ecmascript.pages.academy/task-manager`,
-  AUTHORIZATION: `Basic hgdjfh786kjds55fsldjfls`,
+  AUTHORIZATION: `Basic hgggbnvbvmbbfhgfhjhj1155gfhgfls`,
   CONTENT_TYPE: {"Content-Type": `application/json`},
   STORE_NAME: `${STORE_PREFIX}-${STORE_VER}`,
   SUNC: `tasks/sync`
